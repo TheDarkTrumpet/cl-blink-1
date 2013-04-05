ce379db299f957ea2332d606b82bee1559ca4b2b
David Thole <dthole@gmail.com>
2013-04-05 12:12:12 -0500

    Adding in the defpackage and in-package portions for all areas that need it

--- src/cl-blink-1.lisp
+++ src/cl-blink-1.lisp
@@ -1,3 +1,5 @@
 ; cl-blink-1.lisp
 ; All the functions to control the blink1-tool program
 ; Note, this assumes that blink1-tool is in your path.
+
+(in-package :cl-blink-1)
--- src/package.lisp
+++ src/package.lisp
@@ -1,2 +1,7 @@
 ; package.lisp
 ; Package definition for cl-blink-1
+
+(in-package :cl-user)
+
+(defpackage :cl-blink-1
+  (:use :cl))
